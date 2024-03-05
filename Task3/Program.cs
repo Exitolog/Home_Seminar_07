4f473853e66ed2cd31af6e9e65063cf592e9edde
Sergey <belykov1999@yandex.ru>
2024-03-05 15:32:11 +0300

    Add Home_Seminar_07:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте произвольный массив.
+// Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+void Main()
+{
+
+
+
+    int[] array = { 3, 7, -5, 9, 4, -11, 8 };
+    PrintArray(array);
+
+
+
+    void PrintArray(int[] array, int i = 0)
+    {
+        if (i < array.Length)
+        {
+            PrintArray(array, i + 1);
+            Console.Write(array[i] + " ");
+            //PrintArray(array, i+1);
+        }
+    }
+
+}
+Main();
