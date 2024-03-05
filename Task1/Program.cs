bfb443767d195e1d2b8d51b758f6d7b1fc6824c7
Sergey <belykov1999@yandex.ru>
2024-03-05 10:41:04 +0300

    Add Home_Seminar_07:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+void Main()
+{
+int m = ReadInt("Введите число M: ");
+int n = ReadInt("Введите число N: ");
+PrintNumbers(m,n);
+int ReadInt (string msg)
+{
+Console.WriteLine(msg);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumbers(int m, int n)
+{
+    if (m <= n)
+ {
+if (m == n) return;
+System.Console.Write(m + " ");
+PrintNumbers(m+1, n);
+//System.Console.Write(m + " ");
+}
+else if (m >= n)
+{
+if (m == n) return;
+System.Console.Write(m + " ");
+PrintNumbers(m-1, n);
+//System.Console.Write(m + " ");
+}
+
+}
+}
+Main();
